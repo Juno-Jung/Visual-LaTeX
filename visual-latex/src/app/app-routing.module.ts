ffaebe87b55cc01ed9634f3b48576a13986d52f1
Juno <Laevino@gmail.com>
2020-06-10 10:32:12 -0400

    chore: fixed app-routing so that it compiles.

--- visual-latex/src/app/app-routing.module.ts
+++ visual-latex/src/app/app-routing.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-
+const routes: Routes = [
+  { path: '/dashboard', component: DashboardComponent },
+]
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+    RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
